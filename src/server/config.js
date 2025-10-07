f1d1e24fda23c82802f0f05eb1d294ffd53dd498
linkin <pablogermanjimenez7@gmail.com>
2025-10-07 13:42:44 -0300

    first commit

---
+++ src/server/config.js
@@ -0,0 +1,35 @@
+import express from "express"
+import cors from 'cors'
+import morgan from "morgan"
+import {dirname} from "path"
+import {fileURLToPath} from "url"
+//1- Tomar un puerto
+//2- condfigura los middlewares
+//3- usa las rutas
+
+export default class Server {
+    constructor(){
+        this.app = express()
+        this.port = process.env.PORT || 3001
+        this.middlewares()
+    
+    }
+
+    middlewares(){
+        this.app.use(cors())//permite conexiones remotas o externas 
+        this.app.use(express.json())//permite interpretar los datos que lleguen  en la solicitud  en formato JSON
+        this.app.use(morgan('dev'))//nos ofrece datos extras en la terminal
+        //configurar un archivo estático
+        const __dirname = dirname(fileURLToPath(import.meta.url))
+        console.log(__dirname)
+        console.log(__dirname + "/public")
+        this.app.use(express.static(__dirname + '/public'))
+    }
+
+    listen(){
+        this.app.listen(this.port, ()=>console.info(`Servidor corriendo en puerto ${this.port}`))
+    }
+
+}
+
+
